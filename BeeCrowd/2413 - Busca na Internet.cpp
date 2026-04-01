ea82fe6bca6a6e7cf587a47077534a4e4d21065d
Enrico Zagallo <157983636+EnricoZagallo@users.noreply.github.com>
2026-04-01 12:01:28 -0300

    Add solution for BeeCrowd problem 2413

---
+++ BeeCrowd/2413 - Busca na Internet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ int T;
+ int segundoLink;
+ int primeiroLink;
+ cin >> T;
+ segundoLink = T * 2;
+   primeiroLink = segundoLink*2;
+    cout<< primeiroLink<< endl;
+ 
+    return 0;
+}
